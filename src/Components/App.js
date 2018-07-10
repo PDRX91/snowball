46c9d38e487e0f3fb350b632eae738dd22b17490
Nolan <nemerson17@gmail.com>
2018-07-09 18:52:05 -0700

    Set up create react app and organized folders

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../assets/css/index.css';
+
+class App extends Component {
+
+	render() {
+    	return (
+			<div>
+				Welcome to Snowball
+			</div>
+    	);
+    }
+}
+
+export default App;
